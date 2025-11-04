6561e0f31cfe3dae5c63f93cd346b8ea7a90fa36
WilsonArmando7 <stwvice4@upana.edu.gt>
2025-11-03 21:31:14 -0600

    fix: puerto dinámico para Render

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const __dirname = path.dirname(__filename);
 
 // ✅ Configuración inicial
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor escuchando en el puerto ${PORT}`);
+});
+
 
 app.use(cors());
 app.use(express.json());
